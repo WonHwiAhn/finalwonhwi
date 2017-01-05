cf17e8e12a8f0cfebf4b59ddacda92687d455af3
Allan <myroom9@naver.com>
2017-01-05 22:07:47 +0900

    init heroku

--- web.js
+++ web.js
@@ -1,15 +1,21 @@
+var http = require('http');
+var fs = require('fs');
 var express = require('express');
-var app = express.createServer(express.logger());
-app.get('/', function(request, response){
-response.send('Hello World!');
+var bodyParser = require('body-parser');
+var ejs = require('ejs');
+
+var app = express();
+app.use(bodyParser());
+var router = express.Router();
+app.use(express.static('public'));
+app.use(router);
+
+var port = process.env.PORT || 3000;
+
+router.get("/", function(req, res){
+  res.send("<h1>hello");
 });
-var port = 3000;
-app.listen(port, function()
-{
-console.log("Listening on " + port);
-});
-/*
-app.listen(process.env.PORT || 3000, function(){
-  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-});
-*/
+
+http.createServer(app).listen(port, function(){
+  console.log('server run');
+})
