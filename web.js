f7bbd521303af9265b12a6ae0e10c5115d9fb827
Allan <myroom9@naver.com>
2017-01-05 21:27:58 +0900

    init heroku

--- web.js
+++ web.js
@@ -4,10 +4,12 @@ app.get('/', function(request, response){
 response.send('Hello World!');
 });
 var port = 3000;
-/*app.listen(port, function()
+app.listen(port, function()
 {
 console.log("Listening on " + port);
-});*/
+});
+/*
 app.listen(process.env.PORT || 3000, function(){
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
+*/
